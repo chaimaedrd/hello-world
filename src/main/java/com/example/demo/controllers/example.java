39851d7d185bef8f56091f71179bbcda6f61ccc2
chaimae <dardourichaimae0@gmail.com>
2023-11-30 18:38:32 +0100

    add controller annotation

--- src/main/java/com/example/demo/controllers/example.java
+++ src/main/java/com/example/demo/controllers/example.java
@@ -1,8 +1,10 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class example {
     @GetMapping("/")
     public String mainPage(Model model) {
